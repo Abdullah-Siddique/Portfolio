6fe85cf54625a43346943912f4be53d2f4588ed3
Abdullah-Siddique <137615484+Abdullah-Siddique@users.noreply.github.com>
2024-02-09 23:33:27 +0600

    Add files via upload

---
+++ Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h2 className="center">Projects</h2>
+      {/* Your projects content */}
+    </div>
+  );
+};
+
+export default Projects;
